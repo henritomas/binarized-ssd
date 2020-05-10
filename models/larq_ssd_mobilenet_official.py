cd10958a2ce93567ed79fe7ca2d710f6a1c62f31
henritomas <henri.tomas@eee.upd.edu.ph>
2020-05-10 20:53:52 +0800

    2-stage training added

--- models/larq_ssd_mobilenet_official.py
+++ models/larq_ssd_mobilenet_official.py
@@ -8,7 +8,7 @@ import tensorflow.keras.backend as K
 
 
 #from models.depthwise_conv2d import DepthwiseConvolution2D
-from models.mobilenet_v1_xnor import mobilenet
+from models.mobilenet_v1_xnor_2s import mobilenet
 
 from keras_layers.tensorflow_keras_layer_AnchorBoxes import AnchorBoxes
 from keras_layers.tensorflow_keras_layer_DecodeDetections import DecodeDetections
