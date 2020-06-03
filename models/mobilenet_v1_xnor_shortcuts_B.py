90d58500f70254d77c7f4910c4c00ec7cf9b0ea4
henritomas <henri.tomas@eee.upd.edu.ph>
2020-06-03 16:57:07 +0800

    added skip connections

--- models/mobilenet_v1_xnor_shortcuts_B.py
+++ models/mobilenet_v1_xnor_shortcuts_B.py
@@ -99,7 +99,7 @@ def _depthwise_conv_block_classification(inputs, pointwise_conv_filters, alpha,
 
     x = BatchNormalization(axis=channel_axis, momentum=0.99, epsilon=0.001, name='conv_pw_%d_bn' % block_id)(x)
     
-    if not strides==(2,2) or binary_ds:
+    if not strides==(2,2):
         x = Add()([x, sc])
 
     return x
